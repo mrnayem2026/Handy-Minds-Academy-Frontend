a1eeef75c3c45d394ecd75b68ebee0a1f659363a
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2024-01-28 20:12:36 +0600

    Main layout added by ant desing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import MainLayout from "./components/layout/MainLayout";
 
 function App() {
   return (
     <>
-      <h1 className="text-7xl  flex justify-center items-center  font-bold  text-red-500">
-        empty-react-ts-app
-      </h1>
+     <MainLayout></MainLayout>
     </>
   );
 }
