304f9a33ba5c3d1b9497a5d8271a8c08c227c2dd
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2024-02-18 08:00:35 +0600

    make baseApi,authApi and endpoint for login

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../redux/features/auth/authSlice'
+import { baseApi } from "./api/baseApi";
 export const store = configureStore({
     reducer:{
+        [baseApi.reducerPath]:baseApi.reducer,
         auth: authReducer,
     }
 })
